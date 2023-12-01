de16a206d45f846267fcf1bca87ac9a3b09321ce
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-12-01 16:24:27 +0100

    Initial Commit.

---
+++ src/myapp/MyCharacter.java
@@ -0,0 +1,25 @@
+package myapp;
+
+public class MyCharacter {
+    public String imie;
+    public String klasaPostaci;
+    public String rasa;
+    public  int sila;
+
+    public MyCharacter(String imie, String klasaPostaci, String rasa, int sila)
+    {
+        this.imie = imie;
+        this.klasaPostaci = klasaPostaci;
+        this.rasa = rasa;
+        this.sila = sila;
+    }
+
+    /*public void wyswietlinfo()
+    {
+        System.out.println("Imie: " + imie);
+        System.out.println("Klasa Postaci: " + klasaPostaci);
+        System.out.println("Rasa: " + rasa);
+        System.out.println("Siła: " + sila);
+
+    }*/
+}
