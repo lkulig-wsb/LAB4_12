dffbb322271ff6b0b71b1fe23c3cbd9d653fbd48
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-12-11 09:57:17 +0100

    LAB4_12 Update1 Commit.

--- src/myapp/MyCharacter.java
+++ src/myapp/MyCharacter.java
@@ -1,11 +1,12 @@
 package myapp;
-
 public class MyCharacter {
+    //Dane składowe klasy MyCharacter.
     public String imie;
     public String klasaPostaci;
     public String rasa;
     public  int sila;
 
+    //Konstruktor klasy MyCharacter.
     public MyCharacter(String imie, String klasaPostaci, String rasa, int sila)
     {
         this.imie = imie;
@@ -13,13 +14,11 @@ public class MyCharacter {
         this.rasa = rasa;
         this.sila = sila;
     }
-
     /*public void wyswietlinfo()
     {
         System.out.println("Imie: " + imie);
         System.out.println("Klasa Postaci: " + klasaPostaci);
         System.out.println("Rasa: " + rasa);
         System.out.println("Siła: " + sila);
-
     }*/
 }
--- src/testowanie/TestCharacter.java
+++ src/testowanie/TestCharacter.java
@@ -2,15 +2,15 @@ package testowanie;
 import myapp.MyCharacter;
 public class TestCharacter {
     public static void main(String[] args) {
-        MyCharacter ch1 = new MyCharacter("John","komandos", "człowiek",45);
+        //Utworzenie obiektu klasy MyCharacter(Osoba) + ustawienie wartości dla pól obiektu.
+        MyCharacter ch1 = new MyCharacter("John Rambo","komandos", "człowiek",45);
 
+        //Wyświetlenie informacji o MyCharacter(Osoba) na konsoli.
         System.out.println("--- Info o Postaci ---\n");
-
         System.out.println("Imie: " + ch1.imie);
         System.out.println("Klasa Postaci: " + ch1.klasaPostaci);
-        System.out.println("Rasa: " + ch1.klasaPostaci);
+        System.out.println("Rasa: " + ch1.rasa);
         System.out.println("Siła: " + ch1.sila);
         //ch1.wyswietlinfo();
-
     }
 }
